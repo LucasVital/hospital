603c9dc4ac6723b96317e57aa54a79f93c7f7fa6
Lucas <lucasvital@outlook.com>
2022-10-28 17:32:22 -0400

    wip

--- resources/views/livewire/paciente/navigation.blade.php
+++ resources/views/livewire/paciente/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav class="-mb-px flex space-x-2 mb-6" aria-label="Tabs">
     @foreach($steps as $step)
-        <div class="border-transparent group cursor-pointer inline-flex items-center py-4 px-1 border-b-2 font-medium text-xs md:text-md
+        <div class="border-transparent group cursor-pointer inline-flex items-center py-4 px-1 border-b-2 font-medium text-xs sm:text-sm
         {{ $step->isCurrent() ? 'text-indigo-500 hover:text-indigo-700 hover:border-indigo-300' : '' }}
         {{ $step->isPrevious() ? 'text-indigo-300 hover:text-indigo-600 hover:border-indigo-400' : '' }}
         {{ $step->isNext() ? 'text-gray-400 cursor-not-allowed' : '' }}"
